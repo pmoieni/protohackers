f954cd335ae30c0feff36514819bab686af3ea3f
pmoieni <62774242+pmoieni@users.noreply.github.com>
2025-08-24 00:32:40 +0330

    fix

--- go/one/main.go
+++ go/one/main.go
@@ -57,7 +57,7 @@ func main() {
 			scanner := bufio.NewScanner(conn)
 			for scanner.Scan() {
 				var request req
-				if err := request.UnmarshalJSON(scanner.Bytes()); err != nil {
+				if err := json.Unmarshal(scanner.Bytes(), &request); err != nil {
 					_, err := conn.Write([]byte("bingus"))
 					fatal(err)
 					fatal(conn.Close())
